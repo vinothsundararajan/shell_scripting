c0dd7ab3a5e0abb61d582c83e1cf44f76baae10f
vinoth sundararajan <vinothsundararajan@outlook.com>
2017-06-08 15:46:47 +0530

    backup_cpanel.sh

---
+++ backup_cpanel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+IFS="$"
+
+now=$(date +"%m-%d-%Y")
+
+mkdir -p /mnt/backup/$now
+
+tempdir=/mnt/backup/$now
+
+targetdir=/mnt/backup
+
+archivefile="`date +%Y-%m-%d_%H-%M`_making.tar"
+
+cd /var/cpanel/users
+
+find * | while read CPUSER; do
+
+  echo "Now processing ${CPUSER} ..."
+
+  /scripts/pkgacct ${CPUSER} $tempdir
+  done
+
+# Generating single archive file
+
+tar -cf  ${targetdir}/${archivefile} ${tempdir}
+
+rm -rf $tempdir
